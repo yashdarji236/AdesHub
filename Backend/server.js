b5c43af1b19bff45e47b14a49e7b78231b76844c
Nisarg1223 <darjinisarg49@gmail.com>
2026-08-15 10:52:45 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -13,5 +13,5 @@ connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port now on  ${PORT}`);
 });
